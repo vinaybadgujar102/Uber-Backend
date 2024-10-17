28b1873cc0c9a221d35a23a8b46146b1e5de4961
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-17 11:33:11 +0530

    add basic auth functionality template

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+const authService = require("../services/authService");
+
+const register = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = { register, login };
---
+++ services/authService.js
@@ -0,0 +1,8 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const register = async (userData) => {};
+
+const login = async ({ email, password }) => {};
+
+module.exports = { register, login };
