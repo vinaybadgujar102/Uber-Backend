bef0f8afb871769b2e0c01b8f9579d7df1db76fc
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-17 10:48:07 +0530

    add auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Not authorized" });
+  }
+};
+
+module.exports = authMiddleware;
